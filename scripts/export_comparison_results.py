e02c21b94aa73e037faf724304dda48a7db43cb7
rzfzr <capitao.rafa@gmail.com>
2026-01-31 13:48:25 +0100

    Change minimum created date

--- scripts/export_comparison_results.py
+++ scripts/export_comparison_results.py
@@ -73,7 +73,7 @@ from CounterFactualVisualizer import heatmap_techniques
 WANDB_ENTITY = 'mllab-ts-universit-di-trieste'
 WANDB_PROJECT = 'CounterFactualDPG'
 SELECTED_DATASETS = None  # No filter - fetch all
-MIN_CREATED_AT = "2026-01-28T22:00:00"
+MIN_CREATED_AT = "2026-01-31T22:00:00" # Fetch runs created after this date
 APPLY_EXCLUDED_DATASETS = True
 
 # Output directory
